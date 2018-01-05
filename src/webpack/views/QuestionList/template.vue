f04ee7de78dfb3c0a7eaa0fccf9fd9bfe6147572
admin <visualjupiter@gmail.com>
2018-01-05 22:59:29 +0900

    問題リスト画面にv-containerが設定されていなかったので設定

--- src/webpack/views/QuestionList/template.vue
+++ src/webpack/views/QuestionList/template.vue
@@ -1,23 +1,25 @@
 <template>
-  <v-list>
-    <v-subheader>対象文字列</v-subheader>
-    <template v-for="(question, index) in questions">
-      <v-divider></v-divider>
-      <v-list-tile
-        :to="{
-          name: 'question',
-          params: {
-            category: $route.params.category,
-            id: index,
-          },
-        }"
-      >
-        <v-list-tile-content>
-          <v-list-tile-title>{{ question.title }}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </template>
-  </v-list>
+  <v-container>
+    <v-list>
+      <v-subheader>対象文字列</v-subheader>
+      <template v-for="(question, index) in questions">
+        <v-divider></v-divider>
+        <v-list-tile
+          :to="{
+            name: 'question',
+            params: {
+              category: $route.params.category,
+              id: index,
+            },
+          }"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title>{{ question.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
+  </v-container>
 </template>
 
 <script src="./script.js"></script>
